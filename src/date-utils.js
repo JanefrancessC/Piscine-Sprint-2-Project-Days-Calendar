20b5eadad1414310ef8eaea0a3a09ab559d25179
JanefrancessC <janefrancesschioma@gmail.com>
2026-02-16 17:09:16 +0000

    mapped occurrence to numbers

--- src/date-utils.js
+++ src/date-utils.js
@@ -1,12 +1,50 @@
 /**
- * Calculates the specified date for a commemorative d
+ * Calculates the specified date for a day object from days.json
  * @param {number} year - The year to calculate.
- * @param {object} dayObject - The object from days.json file.
- * @returns {Date} - The calculated Date object.
+ * @param {object} dayObject - The object from JSON
+ * @returns {Date|null} - The calculated Date object.
  */
-export const calculateCommemorativeDates = function (year, dayObject) {
-    const { monthName, dayName, occurrence} = dayObject;
 
-    const month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
+export const calculateCommemorativeDate = function (year, dayObject) {
+  const { monthName, dayName, occurrence } = dayObject;
+
+//   Get index of month and day
+  const monthIndex = months.indexOf(monthName);
+  const weekdayIndex = days.indexOf(dayName);
+
+  if (monthIndex === -1 || weekdayIndex === -1) {
+    throw new Error("Invalid month or day!");
+  }
+
+//   Map occurrences to numbers
+  const occurrenceMapped = {
+    "first": 1,
+    "second": 2,
+    "third": 3,
+    "fourth": 4
+  }
 }
