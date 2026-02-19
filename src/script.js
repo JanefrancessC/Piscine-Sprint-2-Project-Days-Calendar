ca6a0f16c72c45320e78c583bb2036b799a20fba
JanefrancessC <janefrancesschioma@gmail.com>
2026-02-19 09:31:29 +0000

    initialized month and year selectors

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 import { renderCurrentMonth } from "./displayCalendar.js";
+import { initMonthYearSelect } from "./monthYearSelect.js";
 
 // Start the first calendar view.
+initMonthYearSelect();
 renderCurrentMonth();
